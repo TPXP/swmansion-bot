f2c3a36bbb92ac4db0eda9f2fc4e609c73e617e7
Michał Bert <63123542+m-bert@users.noreply.github.com>
2025-06-05 18:02:10 +0200

    Handle special characters in issue labels (#5)
    Currently bot looks for label with the following regex:
    
    ```js
    new RegExp(`${section}`)
    ```
    
    Unfortunately, if label contains special characters (for example link to `gist`), this regex will not work (example in [this PR](https://github.com/software-mansion/react-native-gesture-handler/pull/3546)).
    
    This PR adds `_normalizeSection` function which adds escape characters where they are needed.

--- needs-more-info/IssueTemplateValidator.js
+++ needs-more-info/IssueTemplateValidator.js
@@ -8,12 +8,17 @@ class IssueTemplateValidator {
     this.requiredSections = requiredSections;
   }
 
+  // Uses regex from `escape` https://github.com/LvChengbin/escape/blob/master/src/regexp.js
+  _normalizeSection(section) {
+    return section.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
   _sectionExists(section) {
     return this._getSectionPosition(section) !== -1;
   }
 
   _getSectionPosition(section) {
-    const regexp = new RegExp(`[#]+[ ]+${section}`);
+    const regexp = new RegExp(`[#]+[ ]+${this._normalizeSection(section)}`);
     return this.issueBody.search(regexp);
   }
 
@@ -21,7 +26,8 @@ class IssueTemplateValidator {
   _isSectionEmpty(section) {
     const sectionPosition = this._getSectionPosition(section);
     const sub = this.issueBody.substr(sectionPosition);
-    const sectionStartIndex = sub.search(new RegExp(`${section}`)) + section.length;
+    const sectionStartIndex =
+      sub.search(new RegExp(`${this._normalizeSection(section)}`)) + section.length;
     const nextSectionPos = sub.search(/\n[#]+/);
     const end = nextSectionPos === -1 ? undefined : nextSectionPos;
     const sectionContent = sub.substring(sectionStartIndex, end);
